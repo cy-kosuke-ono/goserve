631af3b71e08bedfa33b3976ba9cf50eb337d40f
cy-kosuke-ono <kosuke_ono@cybird.co.jp>
2017-08-09 23:34:41 +0900

    Short route list

--- route.go
+++ route.go
@@ -8,21 +8,12 @@ import (
 func Router(e *echo.Echo) {
 	render := action.New()
 
-	e.GET("/", render.Hello())
-	e.HEAD("/", render.Hello())
+	methods := []string{"GET", "HEAD"}
 
-	e.GET("/hello", render.Hello())
-	e.HEAD("/hello", render.Hello())
-
-	e.GET("/hello/:name", render.HelloWithName())
-	e.HEAD("/hello/:name", render.HelloWithName())
-
-	e.GET("/hello/:name/json", render.HelloWithNameJSON())
-	e.HEAD("/hello/:name/json", render.HelloWithNameJSON())
-
-	e.GET("/teapod", render.Teapod())
-	e.HEAD("/teapod", render.Teapod())
-
-	e.GET("/route", render.ToJSON(e.Routes()))
-	e.HEAD("/route", render.ToJSON(e.Routes()))
+	e.Match(methods, "/", render.Hello())
+	e.Match(methods, "/hello", render.Hello())
+	e.Match(methods, "/hello/:name", render.HelloWithName())
+	e.Match(methods, "/hello/:name/json", render.HelloWithNameJSON())
+	e.Match(methods, "/teapod", render.Teapod())
+	e.Match(methods, "/route", render.ToJSON(e.Routes()))
 }
